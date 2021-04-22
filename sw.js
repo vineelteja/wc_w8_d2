90dfcf26a461b5ddd161c1a7249c5d70ed7df53d
vineel <email.com>
2021-04-21 19:32:23 -0700

    first commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+var chacheName = "hello-pwa";
+var filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
